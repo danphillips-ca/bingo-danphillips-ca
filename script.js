7bab5eaa3ff925571e7e8a86b3a0111a697dd9b6
Dan Phillips <dan@danphillips.ca>
2024-06-26 22:04:10 -0300

    Delete Cookie

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function toggleFullscreen() {
         bsCollapse.hide();
     }
 }
+
+// Function to delete a cookie by name
+function deleteCookie(name) {
+    // Set the cookie's expiration date to a past date
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    // Reload the webpage
+    location.reload();
+}
