4fe7e2947bfed495d650e6e60bad4ad79aaf6dcc
Dan Phillips <dan@danphillips.ca>
2024-06-27 00:15:48 -0300

    Attached functions to window

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.deleteCookie = function(name) {
     location.reload();
 };
 
-function toggleFullscreen() {
+window.toggleFullscreen = function() {
     var navbarCollapse = document.getElementById('navbarNav');
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
@@ -22,10 +22,9 @@ function toggleFullscreen() {
         });
         bsCollapse.hide();
     }
-}
+};
 
-// Function to shuffle an array
-function shuffle(array) {
+window.shuffle = function(array) {
     console.log('Shuffling array:', array);
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -33,33 +32,29 @@ function shuffle(array) {
     }
     console.log('Shuffled array:', array);
     return array;
-}
+};
 
-// Function to set a cookie
-function setCookie(name, value, days) {
+window.setCookie = function(name, value, days) {
     const date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
+};
 
-// Function to get a cookie by name
-function getCookie(name) {
+window.getCookie = function(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
     return null;
-}
+};
 
-// Function to parse URL parameters
-function getUrlParameter(name) {
+window.getUrlParameter = function(name) {
     const url = new URL(window.location.href);
     const param = url.searchParams.get(name);
     return param ? decodeURIComponent(param) : null;
-}
+};
 
-// Function to initialize the game
-function initializeGame() {
+window.initializeGame = function() {
     if (gameData) {
         document.getElementById('gameTitle').innerText = gameData.title;
 
@@ -99,9 +94,9 @@ function initializeGame() {
             }
         }
     }
-}
+};
 
-function shuffleGameData(data) {
+window.shuffleGameData = function(data) {
     console.log('Shuffling game data:', data);
     for (let key in data) {
         if (Array.isArray(data[key])) {
@@ -109,17 +104,15 @@ function shuffleGameData(data) {
         }
     }
     console.log('Shuffled game data:', data);
-}
+};
 
-// Initialize the game when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('Page loaded. Initializing game...');
     initializeGame();
 });
 
-let gameData = null;
-const cookieData = getCookie('bingoGameData');
-const base64Data = getUrlParameter('data');
+let cookieData = getCookie('bingoGameData');
+let base64Data = getUrlParameter('data');
 
 console.log('Cookie data:', cookieData);
 console.log('URL data:', base64Data);
@@ -137,7 +130,7 @@ try {
                 gameData = JSON.parse(atob(base64Data));
                 console.log('Parsed URL data:', gameData);
                 shuffleGameData(gameData);
-                shuffledGameData = gameData;
+                let shuffledGameData = gameData;
                 console.log('Shuffled new game data:', shuffledGameData);
                 setCookie('bingoGameData', base64Data, shuffledGameData, 7);
                 initializeGame();
@@ -154,7 +147,7 @@ try {
             gameData = JSON.parse(atob(base64Data));
             console.log('Parsed URL data:', gameData);
             shuffleGameData(gameData);
-            shuffledGameData = gameData;
+            let shuffledGameData = gameData;
             console.log('Shuffled new game data:', shuffledGameData);
             setCookie('bingoGameData', base64Data, shuffledGameData, 7);
             initializeGame();
